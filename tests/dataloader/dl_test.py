568549fcd56d3f94c455ac00b1081c4ba0d22f18
Aman <aman.swar001@gmail.com>
2025-04-19 18:42:53 +0530

    test for simclr aug

--- tests/dataloader/dl_test.py
+++ tests/dataloader/dl_test.py
@@ -1,16 +1,15 @@
 from torchssl.dataset.ssldataloader import SSLDataloader
-
+from torchssl.dataset.sslaug import SimclrAug
 
 path_dir = "tests/test_data/unlabeled_images"
 
+
 dataloader = SSLDataloader(
     data_dir=path_dir,
-    augmentation=None,
+    augmentation=SimclrAug(img_size=224),
     batch_size=32,
     num_workers=3
 )
-
-
 
 for i in dataloader:
     print(i)
