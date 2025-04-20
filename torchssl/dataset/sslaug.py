6d37ed429337af1912b55ec42f0869b51cd7298d
Aman <aman.swar001@gmail.com>
2025-04-20 14:43:44 +0530

    Add moco augmentation

--- torchssl/dataset/sslaug.py
+++ torchssl/dataset/sslaug.py
@@ -49,8 +49,33 @@ class SimclrAug:
 #---------------------------------------------
 #MOCO
 
+class MocoAug:
 
+    def __init__(
+            self,
+            img_size=224,
+            s=1.0
+    ):
+        color_jitter = transforms.ColorJitter(
+            brightness=0.4,
+            contrast=0.4,
+            saturation=0.4,
+            hue=0.1
+        )
 
+        self.base_trans = transforms.Compose([
+                transforms.RandomResizedCrop(size=img_size, scale=(0.2, 1.0)),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.RandomApply([color_jitter], p=0.8),
+                transforms.RandomGrayscale(p=0.2),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225]),
+            ])
+    def __call__(self, img):
+        q = self.base_transform(img)
+        k = self.base_transform(img)
+        return q, k
 
 
 
