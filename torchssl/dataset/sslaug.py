830c74e893e3b9db99701e96b926bf2baf23cfeb
Aman <aman.swar001@gmail.com>
2025-04-20 17:45:10 +0530

    removed normalization for mnist

--- torchssl/dataset/sslaug.py
+++ torchssl/dataset/sslaug.py
@@ -32,7 +32,7 @@ class SimclrAug:
                 ),
 
                 transforms.ToTensor(),
-                transforms.Normalize(mean=self.mean, std=self.std)
+                # transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
             ]   
         )
 
